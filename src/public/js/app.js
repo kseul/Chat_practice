9a8da41126c084fa8c27bbaea99924f49ed6f1b0
seul <kseul06@gmail.com>
2022-08-30 14:48:14 +0900

    ADD : front & back 메세지 주고받기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-alert('hi');
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener('open', () => {
+  console.log('서버와 연결 💫');
+});
+
+socket.addEventListener('message', (message) => {
+  console.log('서버로부터 받은 메세지 📝 ', message.data);
+});
+
+socket.addEventListener('close', () => {
+  console.log('서버로부터 연결 끊김 🔥');
+});
+
+setTimeout(() => {
+  socket.send('브라우저에서 메세지 보내기!');
+}, 5000);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from 'http';
+import WebSocket from 'ws';
 import express, { application } from 'express';
 
 const app = express();
@@ -7,6 +9,23 @@ app.set('views', __dirname + '/public/views');
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => res.render('home'));
+app.get('/*', (req, res) => res.redirect('/'));
 
 const handleListen = () => console.log('hello');
-app.listen(3000, handleListen);
+
+const server = http.createServer(app); // http 서버
+const wss = new WebSocket.Server({ server }); // WebSocket 서버
+
+wss.on('connection', (socket) => {
+  // console.log(socket);
+  console.log('클라이언트 / 브라우저와 연결 🌟');
+  socket.send('hi!!!');
+  socket.on('close', () =>
+    console.log('클라이언트 / 브라우저로부터 연결이 끊김🔥')
+  );
+  socket.on('message', (message) => {
+    console.log(message.toString());
+  });
+});
+
+server.listen(3000, handleListen);
